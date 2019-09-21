7cee7d92fdb4f4855032250b87157649e8200009
liyasthomas <liyascthomas@gmail.com>
2019-09-22 00:39:09 +0530

    :zap:

--- index.js
+++ index.js
@@ -1,243 +1,240 @@
 /*
 http://api.giphy.com/v1/gifs/search?q=coding%20train&api_key=dc6zaTOxFJmzC&limit=5
 */
-var api = "https://api.giphy.com/v1/";
-var apiKey = "&api_key=59tpzgCcHAbMjPRqDAUklk79IyBTdF75";
-var playOrPause = "Auto play: ON";
-var links = [];
+const api = 'https://api.giphy.com/v1/';
+const apiKey = '&api_key=59tpzgCcHAbMjPRqDAUklk79IyBTdF75';
+let playOrPause = 'Auto play: ON';
+let links = [];
 
-function loadJSON(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.responseType = "json";
-  xhr.onload = function() {
-    var status = xhr.status;
+function loadJSON (url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true)
+  xhr.responseType = 'json'
+  xhr.onload = () => {
+    const status = xhr.status;
     if (status === 200) {
-      callback(xhr.response, null);
+      callback(xhr.response, null)
     } else {
-      callback(xhr.response, status);
+      callback(xhr.response, status)
     }
-  };
-  xhr.send();
-}
-
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
   }
-  return color;
+  xhr.send()
 }
 
-function addImage(title, gifImage, gifSmall, gifFull, ratio_WidthHeight) {
-  var newImage = document.createElement("IMG");
-  newImage.src = playOrPause == "Auto play: ON" ? gifSmall : gifImage;
-  newImage.title = title;
-  newImage.textContent = links.length;
-  if (searchType() == "gifs/search?" || searchType() == "gifs/trending?")
-    newImage.style.backgroundColor = getRandomColor();
-  else newImage.style.backgroundColor = "transparent";
-
-  var addTo = getMinHeightElement(["images"]);
-  document.getElementById(addTo.id).appendChild(newImage);
-
-  links.push({ image: gifImage, small: gifSmall, full: gifFull });
-  console.log(links.length);
+function getRandomColor () {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
 }
 
-function getMinHeightElement(arr) {
-  var min = document.getElementById(arr[0]).offsetHeight;
-  var result = arr[0];
-  for (var i = 1; i < arr.length; i++) {
-    var h = document.getElementById(arr[i]).offsetHeight;
+function addImage (title, gifImage, gifSmall, gifFull, ratio_WidthHeight) {
+  const newImage = document.createElement('IMG');
+  newImage.src = playOrPause == 'Auto play: ON' ? gifSmall : gifImage
+  newImage.title = title
+  newImage.textContent = links.length
+  newImage.classList.add('img')
+  if (searchType() == 'gifs/search?' || searchType() == 'gifs/trending?') { newImage.style.backgroundColor = getRandomColor() } else newImage.style.backgroundColor = 'transparent'
+
+  const addTo = getMinHeightElement(['images']);
+  document.getElementById(addTo.id).appendChild(newImage)
+
+  links.push({ image: gifImage, small: gifSmall, full: gifFull })
+  console.log(links.length)
+}
+
+function getMinHeightElement (arr) {
+  let min = document.getElementById(arr[0]).offsetHeight;
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    const h = document.getElementById(arr[i]).offsetHeight;
     if (h < min) {
-      min = h;
-      result = arr[i];
+      min = h
+      result = arr[i]
     }
   }
-  return { id: result, value: min };
+  return { id: result, value: min }
 }
 
-function searchType() {
-  return document.getElementById("sType").selectedOptions[0].value;
+function searchType () {
+  return document.getElementById('sType').selectedOptions[0].value
 }
 
-function searchGiphy(giphyName) {
-  var query = "&q=" + giphyName;
-  var limit = "&limit=" + 25;
-  var url = api + searchType() + apiKey + limit + query;
-  loadJSON(url, function(giphys, status) {
+function searchGiphy (giphyName) {
+  const query = `&q=${giphyName}`;
+  const limit = `&limit=${25}`;
+  const url = api + searchType() + apiKey + limit + query;
+  loadJSON(url, ({data}, status) => {
     if (status === null) {
-      if (giphys.data.length < 1) {
-        window.alert("Dont have data for this giphy name");
+      if (data.length < 1) {
+        window.alert('Dont have data for this giphy name')
       } else {
-        deleteImages();
-        for (var i = 0; i < giphys.data.length; i++) {
+        deleteImages()
+        for (let i = 0; i < data.length; i++) {
           addImage(
-            giphys.data[i].title,
-            giphys.data[i].images.fixed_height_still.url,
-            giphys.data[i].images.fixed_height_small.url,
-            giphys.data[i].images.original.url,
-            giphys.data[i].images.fixed_height_small.height /
-              giphys.data[i].images.fixed_height_small.width
-          );
+            data[i].title,
+            data[i].images.fixed_height_still.url,
+            data[i].images.fixed_height_small.url,
+            data[i].images.original.url,
+            data[i].images.fixed_height_small.height /
+              data[i].images.fixed_height_small.width
+          )
         }
       }
     }
-  });
+  })
 
-  if (searchType() == "gifs/search?")
-    for (var i = 25; i < window_WidthHeight().height / 20; i++) {
-      addRandom(giphyName, "gifs");
+  if (searchType() == 'gifs/search?') {
+    for (let i = 25; i < window_WidthHeight().height / 20; i++) {
+      addRandom(giphyName, 'gifs')
     }
+  }
 }
 
-function addRandom(giphyName, type) {
-  var tag = "&tag=" + giphyName;
-  var url = api + type + "/random?" + apiKey + tag;
-  loadJSON(url, function(giphys, status) {
-    if (status === null)
+function addRandom (giphyName, type) {
+  const tag = `&tag=${giphyName}`;
+  const url = `${api + type}/random?${apiKey}${tag}`;
+  loadJSON(url, ({data}, status) => {
+    if (status === null) {
       addImage(
-        giphys.data.title,
-        giphys.data.images.fixed_height_still.url,
-        giphys.data.images.fixed_height_small.url,
-        giphys.data.images.original.url,
-        giphys.data.images.fixed_height_small.height /
-          giphys.data.images.fixed_height_small.width
-      );
-  });
+        data.title,
+        data.images.fixed_height_still.url,
+        data.images.fixed_height_small.url,
+        data.images.original.url,
+        data.images.fixed_height_small.height /
+          data.images.fixed_height_small.width
+      )
+    }
+  })
 }
 
-function changeImage() {
-  var x = document.getElementById("inputS").value;
+function changeImage () {
+  const x = document.getElementById('inputS').value;
   if (
-    (x != "" &&
-      searchType() != "gifs/trending?" &&
-      searchType() != "stickers/trending?") ||
-    searchType() == "gifs/trending?" ||
-    searchType() == "stickers/trending?"
-  )
-    searchGiphy(x);
+    (x != '' &&
+      searchType() != 'gifs/trending?' &&
+      searchType() != 'stickers/trending?') ||
+    searchType() == 'gifs/trending?' ||
+    searchType() == 'stickers/trending?'
+  ) { searchGiphy(x) }
 }
 
-function deleteImages() {
-  console.clear();
-  links = [];
-  var imas = document.getElementsByTagName("img");
-  for (var i = imas.length - 1; i >= 0; i--) {
-    imas[i].parentNode.removeChild(imas[i]);
+function deleteImages () {
+  console.clear()
+  links = []
+  const imas = document.getElementsByClassName('img');
+  for (let i = imas.length - 1; i >= 0; i--) {
+    imas[i].parentNode.removeChild(imas[i])
   }
 }
 
-function playPauseImage() {
-  playOrPause =
-    playOrPause == "Auto play: OFF" ? "Auto play: ON" : "Auto play: OFF";
-  var imgs = document.getElementsByTagName("img");
-  for (var i = imgs.length - 1; i >= 0; i--) {
-    var linksI = links[imgs[i].textContent];
-    imgs[i].src = playOrPause == "Auto play: ON" ? linksI.small : linksI.image;
+function playPauseImage () {
+  playOrPause = playOrPause == 'Auto play: OFF' ? 'Auto play: ON' : 'Auto play: OFF'
+  const imgs = document.getElementsByClassName('img');
+  for (let i = imgs.length - 1; i >= 0; i--) {
+    const linksI = links[imgs[i].textContent];
+    imgs[i].src = playOrPause == 'Auto play: ON' ? linksI.small : linksI.image
   }
-  var but = document.getElementById("playPauseBut");
-  but.textContent = playOrPause;
+  const but = document.getElementById('playPauseBut');
+  but.textContent = playOrPause
 }
 
-function window_WidthHeight() {
-  var w =
+function window_WidthHeight () {
+  const w =
     window.innerWidth ||
     document.documentElement.clientWidth ||
     document.body.clientWidth;
-  var h =
+  const h =
     window.innerHeight ||
     document.documentElement.clientHeight ||
     document.body.clientHeight;
-  return { width: w, height: h };
+  return { width: w, height: h }
 }
 
-function copyToClipboard(str) {
+function copyToClipboard (str) {
   // https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-  var el = document.createElement("textarea");
-  el.value = str;
-  el.setAttribute("readonly", "");
-  el.style.position = "absolute";
-  el.style.left = "-9999px";
-  document.body.appendChild(el);
-  var selected =
+  const el = document.createElement('textarea');
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  const selected =
     document.getSelection().rangeCount > 0
       ? document.getSelection().getRangeAt(0)
       : false;
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
   if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
+    document.getSelection().removeAllRanges()
+    document.getSelection().addRange(selected)
   }
 }
 
-window.onload = function() {
-  var w = window_WidthHeight().width;
-  var h = window_WidthHeight().height;
+window.onload = () => {
+  const w = window_WidthHeight().width;
+  const h = window_WidthHeight().height;
 
   // add more gifs when scroll
-  document.getElementById("imgArea").addEventListener("scroll", e => {
-    var ele = e.target;
+  document.getElementById('imgArea').addEventListener('scroll', ({target}) => {
+    const ele = target;
     if (ele.scrollHeight - ele.scrollTop <= ele.clientHeight + h / 4) {
-      var x = document.getElementById("inputS").value;
-      if (searchType() == "gifs/search?") addRandom(x, "gifs");
+      const x = document.getElementById('inputS').value;
+      if (searchType() == 'gifs/search?') addRandom(x, 'gifs')
     }
-  });
+  })
 
-  //event
+  // event
   document
-    .getElementById("inputS")
-    .addEventListener("change", e => changeImage());
+    .getElementById('inputS')
+    .addEventListener('change', e => changeImage())
   document
-    .getElementById("sType")
-    .addEventListener("change", e => changeImage());
+    .getElementById('sType')
+    .addEventListener('change', e => changeImage())
   document
-    .getElementById("playPauseBut")
-    .addEventListener("click", e => playPauseImage());
+    .getElementById('playPauseBut')
+    .addEventListener('click', e => playPauseImage())
 
-  document.getElementById("shareFb").addEventListener("click", e => {
-    window.open("https://facebook.com/sharer/sharer.php?u=" + e.target.src);
-  });
-  document.getElementById("shareTwitter").addEventListener("click", e => {
-    window.open("https://twitter.com/intent/tweet?text=" + e.target.src);
-  });
-  document.getElementById("shareLink").addEventListener("click", e => {
-    copyToClipboard(e.target.src);
-  });
+  document.getElementById('shareFb').addEventListener('click', ({target}) => {
+    window.open(`https://facebook.com/sharer/sharer.php?u=${target.getAttribute('data')}`)
+  })
+  document.getElementById('shareTwitter').addEventListener('click', ({target}) => {
+    window.open(`https://twitter.com/intent/tweet?text=${target.getAttribute('data')}`)
+  })
+  document.getElementById('shareLink').addEventListener('click', ({target}) => {
+    copyToClipboard(target.getAttribute('data'))
+  })
 
-  //event
-  imgArea.addEventListener("mouseover", e => {
-    if (e.target.matches("img")) {
-      var eles = document.getElementsByTagName("a");
-      for (var i = 0; i < eles.length; i++) {
-        eles[i].style.top = (e.target.y + 16) + "px";
-        eles[i].style.left = e.target.x + 32 * i + "px";
-        eles[i].src = links[e.target.textContent].full;
+  // event
+  imgArea.addEventListener('mouseover', ({target}) => {
+    if (target.matches('img')) {
+      const eles = document.getElementsByClassName('a');
+      for (let i = 0; i < eles.length; i++) {
+        eles[i].style.top = `${target.y + 16}px`
+        eles[i].style.left = `${target.x + 32 * i}px`
+        eles[i].setAttribute('data', target.src)
       }
 
-      if (playOrPause == "Auto play: OFF")
-        e.target.src = links[e.target.textContent].small;
+      if (playOrPause == 'Auto play: OFF') { target.src = links[target.textContent].small }
     }
-  });
-  imgArea.addEventListener("mouseout", e => {
-    if (e.target.matches("img") && playOrPause == "Auto play: OFF") {
-      e.target.src = links[e.target.textContent].image;
+  })
+  imgArea.addEventListener('mouseout', ({target}) => {
+    if (target.matches('img') && playOrPause == 'Auto play: OFF') {
+      target.src = links[target.textContent].image
     }
-  });
+  })
 
-  imgArea.addEventListener("click", e => {
-    if (e.target.matches("img")) {
-      window.open(links[e.target.textContent].full);
+  imgArea.addEventListener('click', ({target}) => {
+    if (target.matches('img')) {
+      window.open(links[target.textContent].full)
     }
-  });
+  })
 
-  //add firt random gif
-  console.log("load " + h / 20 + " firt image");
-  for (var i = 0; i < h / 20; i++) {
-    addRandom("hello", "gifs");
+  // add firt random gif
+  for (let i = 0; i < h / 20; i++) {
+    addRandom('hello', 'gifs')
   }
-};
+}
